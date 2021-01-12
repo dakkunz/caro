005e3ccd9f95f257ffce26f7ab12d25466efbdaf
Tran Quoc Toan <37733986+yato511@users.noreply.github.com>
2021-01-12 16:48:44 +0700

    refactor

--- src/components/Game/components/Timer/index.jsx
+++ src/components/Game/components/Timer/index.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 import { useEventTime } from "@/hooks/useEvent";
-import { Statistic } from "antd";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 import { useEffect } from "react";
 
 const Timer = (props) => {
-	// const [countDownTimer, setCountDownTimer] = useState();
-
-	// useEffect(() => {
-	// 	console.log("settimeer", countDownTimer);
-	// }, [countDownTimer]);
-
-	// useEventTime.removeAllListeners();
-
 	const { roomInfo: { time = 0 } = {} } = useSelector((state) => state.room);
 	const [count, setCount] = useState(time);
-
-	// const { Countdown } = Statistic;
 
 	useEffect(() => {
 		const counter = setInterval(() => {
@@ -45,14 +34,7 @@ const Timer = (props) => {
 		props.setIsOverTime(true);
 	};
 
-	return (
-		<div>{count}</div>
-		// <Countdown
-		// 	value={Date.now() + time * 1000}
-		// 	format="ss"
-		// 	onFinish={handleOverTime}
-		// />
-	);
+	return <div>{count}</div>;
 };
 
 export default Timer;

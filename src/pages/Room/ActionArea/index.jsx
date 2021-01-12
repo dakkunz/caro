61198989530300948bc6d35aba1306b67a21a46e
Tran Quoc Toan <37733986+yato511@users.noreply.github.com>
2021-01-13 06:15:29 +0700

    disable button play

--- src/pages/Room/ActionArea/index.jsx
+++ src/pages/Room/ActionArea/index.jsx
@@ -1,7 +1,8 @@
+import useAuth from "@/hooks/useAuth";
 import useSocket from "@/hooks/useSocket";
 import ActionAreaSocket from "@/pages/Room/ActionAreaSocket";
 import { ArrowLeftOutlined, SmileOutlined } from "@ant-design/icons";
-import { Button } from "antd";
+import { Button, message } from "antd";
 import React from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -10,7 +11,10 @@ import "./style.scss";
 const ActionArea = () => {
 	const socket = useSocket();
 	const { roomInfo } = useSelector((state) => state.room);
+	const { user } = useAuth();
 	const { replace } = useHistory();
+
+	const isHost = user.sub === roomInfo.host.sub;
 
 	return (
 		<div className="action-area-wrapper">
@@ -23,16 +27,19 @@ const ActionArea = () => {
 				}}
 			>
 				<ArrowLeftOutlined />
-				Leave Room
+				Rời phòng
 			</Button>
 			<Button
 				type="primary"
+				disabled={!isHost}
 				onClick={() => {
-					socket.emit("start-game-request");
+					if (roomInfo.players.X !== null && roomInfo.players.O !== null)
+						socket.emit("start-game-request");
+					else message.error("Chưa đủ người");
 				}}
 			>
 				<SmileOutlined />
-				Play
+				Bắt đầu
 			</Button>
 			{socket && <ActionAreaSocket />}
 		</div>
